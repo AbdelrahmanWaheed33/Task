15a288b46ce85e05c2a7f853da876fd0f990d767
A_waheed33 <abdowaheed33@gmail.com>
2022-07-11 20:34:20 +0200

    fix style

--- src/components/question.vue
+++ src/components/question.vue
@@ -20,8 +20,8 @@
           }}</b-form-radio>
         </b-form-radio-group>
       </b-card-text>
-      <b-button @click="goBack">Back</b-button>
-      <b-button @click="addAnswer">Next</b-button>
+      <b-button class="mx-4" @click="goBack">Back</b-button>
+      <b-button class="mx-4" @click="addAnswer">Next</b-button>
     </b-card>
   </b-container>
 </template>
